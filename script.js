172efc61ee323264bc0beeb2b5b2a48df58cdaf1
Yang Taehwan <taehwan920@gmail.com>
2020-03-12 14:46:15 +0900

    devEnv is ready

---
+++ script.js
@@ -0,0 +1,11 @@
+function submit() {
+    fetch('', {
+        method: '',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({})
+    })
+        .then(res => res.json())
+        .then(ret => {
+            // Update Element
+        });
+};
