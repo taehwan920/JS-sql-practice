9c1462dd54fc775e082b775ccabbad60b2d50544
Yang Taehwan <taehwan920@gmail.com>
2020-03-12 20:24:29 +0900

    Done!

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 function submit() {
-    fetch('', {
-        method: '',
+    let checkedData = { answers: [] };
+    let checked = document.querySelectorAll(".ck:checked");
+    function checkCheck() {
+        if ((document.querySelector(".ahn").checked) && (checked.length === 0)) {
+            return checkedData = { answers: ["안창호"] };
+        }
+    };
+    checkCheck();
+    fetch('/submit', {
+        method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({})
+        body: JSON.stringify(checkedData)
     })
         .then(res => res.json())
         .then(ret => {
-            // Update Element
-        });
+            ret ?
+                document.getElementById("result").innerHTML = "정답입니다." :
+                document.getElementById("result").innerHTML = "오답입니다.";
+        })
 };
